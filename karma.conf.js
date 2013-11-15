9b7ae0583e8b1a6c16c50c0625c7485987e2127d
Kenta Mori <heiwabanzai@gmail.com>
2013-11-15 17:19:20 +0900

    Update karma test config

--- karma.conf.js
+++ karma.conf.js
@@ -1,80 +1,80 @@
 // Karma configuration
-// Generated on Mon Jun 24 2013 09:58:37 GMT+0900 (JST)
+// Generated on Fri Nov 15 2013 17:00:07 GMT+0900 (JST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+    // frameworks to use
+    frameworks: ['mocha'],
 
 
-// list of files / patterns to load in the browser
-files = [
-  MOCHA,
-  MOCHA_ADAPTER,
-  'lib/*.js',
-  'src/*.js',
-  'test/*.js'
-];
+    // list of files / patterns to load in the browser
+    files: [
+      'lib/*.js',
+      'src/*.js',
+      'test/*.js'
+    ],
 
 
-// list of files to exclude
-exclude = [
-  
-];
+    // list of files to exclude
+    exclude: [
+      
+    ],
 
 
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['dots'];
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['dots', 'coverage'],
 
 
-// web server port
-port = 9876;
+    // web server port
+    port: 9876,
 
 
-// cli runner port
-runnerPort = 9100;
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
 
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera (has to be installed with `npm install karma-opera-launcher`)
+    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+    // - PhantomJS
+    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+    browsers: ['Firefox', 'PhantomJS'],
 
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Firefox', 'PhantomJS'];
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
 
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false,
 
+    // Avail of coverage
+    preprocessors: {
+      'src/*.js': ['coverage']
+    },
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
-
-// Avail of coverage
-preprocessors = {
-  'src/*.js': 'coverage'
+    coverageReporter: {
+      type: 'html',
+      dir : 'coverage/'
+    }
+  });
 };
-reporters = ['progress', 'coverage'];
-
-coverageReporter = {
-  type: 'html',
-  dir : 'coverage/'
-};
-
